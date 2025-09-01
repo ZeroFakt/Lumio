82da0c462c239773891e6de4eb6f4e2e71c01657
aimi <aimi@MacBook-Pro-aimi.local>
2025-09-02 00:46:48 +0500

    Add file gitignore and bot.py

---
+++  bot.py
@@ -0,0 +1,12 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+async def main():
+    bot = Bot(token="")
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
